79c5ca6e3d4ebbcd01005b8e67cf89eac8202a1a
Richard <programmingislife23@gmail.com>
2024-05-20 18:39:06 -0300

    Inicio do projeto Adicionando algumas coisas importantes com a conexão com o banco de dados. Adicionando mais alguns paramêtros importantes, e criando nova classe "MovieController" Alterando parametro do método na classe, e forma de retorno Criando interface para o repositório Criando referencia da classe MovieService na classe MovieController Mongo db corretamente conectado ao framework, consegui mostrar na página o data Tentando retirar do banco de dados um filme

--- movies/src/main/java/Dev/Richard/movies/MovieService.java
+++ movies/src/main/java/Dev/Richard/movies/MovieService.java
@@ -1,10 +1,12 @@
 package Dev.Richard.movies;
 
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,4 +16,8 @@ public class MovieService {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
+  }
 }
--- movies/src/main/java/Dev/Richard/movies/MoviesApplication.java
+++ movies/src/main/java/Dev/Richard/movies/MoviesApplication.java
@@ -1,13 +1,19 @@
 package Dev.Richard.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {SpringApplication.run(MoviesApplication.class, args);}
+
+
 
 
 }
