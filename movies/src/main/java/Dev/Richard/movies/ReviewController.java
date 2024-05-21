555033816f51e19df2faf5d7f3f4918c6047c254
Richard <programmingislife23@gmail.com>
2024-05-21 20:19:03 -0300

    Inicio do projeto Adicionando algumas coisas importantes com a conexão com o banco de dados. Adicionando mais alguns paramêtros importantes, e criando nova classe "MovieController" Alterando parametro do método na classe, e forma de retorno Criando interface para o repositório Criando referencia da classe MovieService na classe MovieController Mongo db corretamente conectado ao framework, consegui mostrar na página o data Tentando retirar do banco de dados um filme Mudando de object id para imdbId modo de localização do filme Adicionando mais recursos para o processo de review dos filmes

---
+++ movies/src/main/java/Dev/Richard/movies/ReviewController.java
@@ -0,0 +1,12 @@
+package Dev.Richard.movies;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class ReviewController {
+
+
+}
--- movies/src/main/java/Dev/Richard/movies/Reviews.java
+++ movies/src/main/java/Dev/Richard/movies/Reviews.java
@@ -17,4 +17,8 @@ public class Reviews {
     @Id
     private ObjectId id;
     private String body;
+
+    public Reviews(String body) {
+        this.body = body;
+    }
 }
