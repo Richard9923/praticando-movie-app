4ac9d781a982f9fce728d7b642fd3c0e1277ef9c
Richard <programmingislife23@gmail.com>
2024-05-26 10:11:26 -0300

    Inicio do projeto Adicionando algumas coisas importantes com a conexão com o banco de dados. Adicionando mais alguns paramêtros importantes, e criando nova classe "MovieController" Alterando parametro do método na classe, e forma de retorno Criando interface para o repositório Criando referencia da classe MovieService na classe MovieController Mongo db corretamente conectado ao framework, consegui mostrar na página o data Tentando retirar do banco de dados um filme Mudando de object id para imdbId modo de localização do filme Adicionando mais recursos para o processo de review dos filmes Progredindo com o projeto

--- movies/src/main/java/Dev/Richard/movies/ReviewController.java
+++ movies/src/main/java/Dev/Richard/movies/ReviewController.java
@@ -1,12 +1,27 @@
 package Dev.Richard.movies;
 
 
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Reviews> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Reviews>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
 
 }
